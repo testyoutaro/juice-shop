396062be84c1319908a40d5dfae05fa22c79c0dc
testyoutaro <testtaro820@gmail.com>
2023-12-13 16:33:42 +0900

    fix traversal

--- data/datacreator.ts
+++ data/datacreator.ts
@@ -38,7 +38,8 @@ const entities = new Entities()
 const readFile = util.promisify(fs.readFile)
 
 function loadStaticData (file: string) {
-  const filePath = path.resolve('./data/static/' + file + '.yml')
+  //const filePath = path.resolve('./data/static/' + file + '.yml')
+  const filePath = path.resolve(__dirname, 'data', 'static', file + '.yml')
   return readFile(filePath, 'utf8')
     .then(safeLoad)
     .catch(() => logger.error('Could not open file: "' + filePath + '"'))
